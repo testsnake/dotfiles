3c8a6aa6f68999aa104be1f683669625105e6429
Emma MB <emma@meredithblack.com>
2024-11-12 19:33:03 -0800

    Update run_onchange_before_programs.sh

--- .chezmoiscripts/run_onchange_before_programs.sh
+++ .chezmoiscripts/run_onchange_before_programs.sh
@@ -1,3 +1,25 @@
 #!/bin/sh
 
-echo "hi2"
+# Main programs I use on every desktop
+
+pacman -S \
+    firefox \
+    fish \
+    fisher \
+    gimp \
+    github-desktop-bin \
+    handbrake \
+    obs-studio \
+    signal-desktop \
+    steam \
+    timeshift \
+    ungoogled-chromium-bin \
+    vesktop \
+    visutal-studio-code-bin \
+    wine \
+    zellij
+
+
+
+
+
